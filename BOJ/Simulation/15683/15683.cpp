88f2edb351b6e68d05c23e7b531a83ed0aaa39ac
Maz2r <tigerinmaze2315@gmail.com>
2025-09-24 17:35:33 +0900

    FEAT: BOJ-15683

---
+++ BOJ/Simulation/15683/15683.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int board[10][10];
+int N, M;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> N >> M;
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < M; j++) {
+      cin >> board[i][j];
+    }
+  }
+
+    return 0;
+}
