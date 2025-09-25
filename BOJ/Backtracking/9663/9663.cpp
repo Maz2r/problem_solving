543ce1b51f70db28deefc579cbed71febe9e1158
Maz2r <tigerinmaze2315@gmail.com>
2025-09-25 13:41:49 +0900

    FEAT: BOJ-9663

---
+++ BOJ/Backtracking/9663/9663.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int board[20][20];
+int N;
+
+int func(int nx);
+
+int main(void) {
+  cin >> N;
+
+  return 0;
+}
+
+int func(int nx) {
+  if (nx == N) return 1;
+
+  for (int i = 0; i < N; i++) {
+  }
+
+  return;
+}
