e935cb626b0949fe0636b092b665590003293ed8
Maz2r <tigerinmaze2315@gmail.com>
2026-04-07 14:06:16 +0900

    FEAT: BOJ-2206

--- BOJ/BFS/2206/2206.cpp
+++ BOJ/BFS/2206/2206.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include <utility>
 #include <queue>
+#include <string>
+#include <utility>
 
 using namespace std;
 
@@ -8,7 +9,7 @@ using namespace std;
 #define Y second
 
 int map[1003][1003];
-int dist[1003][1003];
+int dist[2][1003][1003];
 int N, M;
 
 int dx[4] = {-1, 1, 0, 0};
@@ -19,18 +20,21 @@ int main(void) {
   cin.tie(0);
 
   cin >> N >> M;
+  string s;
   for (int i = 0; i < N; i++) {
+    cin >> s;
     for (int j = 0; j < M; j++) {
-      cin >> map[i][j];
-      dist[i][j] = 0;
+      map[i][j] = s[j] - '0';
+      dist[0][i][j] = 0;
+      dist[1][i][j] = 0;
     }
   }
 
-  queue<pair<bool, pair<int, int>>> Q;
+  queue<pair<int, pair<int, int>>> Q;
 
-  dist[0][0] = 1;
-  Q.push({false, {0, 0}});
-  while(!Q.empty()) {
+  dist[0][0][0] = 1;
+  Q.push({0, {0, 0}});
+  while (!Q.empty()) {
     pair<bool, pair<int, int>> cur = Q.front();
     Q.pop();
 
@@ -39,14 +43,45 @@ int main(void) {
       int ny = cur.Y.Y + dy[dir];
 
       if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-      
-      if (cur.X) {
-        if ((map[nx][ny] == 1) || (dist[nx][ny] != 0 && )) continue;
-      }
-      else {
 
+      if (cur.X) {
+        if ((map[nx][ny] == 1) ||
+            (dist[cur.X][nx][ny] != 0 &&
+             dist[cur.X][nx][ny] <= dist[cur.X][cur.Y.X][cur.Y.Y] + 1))
+          continue;
+
+        dist[cur.X][nx][ny] = dist[cur.X][cur.Y.X][cur.Y.Y] + 1;
+        Q.push({cur.X, {nx, ny}});
+      } else {
+        if (map[nx][ny] == 0) {
+          if (dist[cur.X][nx][ny] != 0 &&
+              dist[cur.X][nx][ny] <= dist[cur.X][cur.Y.X][cur.Y.Y] + 1)
+            continue;
+
+          dist[cur.X][nx][ny] = dist[cur.X][cur.Y.X][cur.Y.Y] + 1;
+          Q.push({cur.X, {nx, ny}});
+        } else {
+          if (dist[1][nx][ny] != 0 &&
+              dist[1][nx][ny] <= dist[cur.X][cur.Y.X][cur.Y.Y] + 1)
+            continue;
+
+          dist[1][nx][ny] = dist[cur.X][cur.Y.X][cur.Y.Y] + 1;
+          Q.push({1, {nx, ny}});
+        }
       }
     }
+  }
+
+  if ((dist[0][N - 1][M - 1] == 0) && (dist[1][N - 1][M - 1] == 0)) {
+    cout << -1;
+  } else if ((dist[0][N - 1][M - 1] != 0) && (dist[1][N - 1][M - 1] == 0)) {
+    cout << dist[0][N - 1][M - 1];
+  } else if ((dist[0][N - 1][M - 1] == 0) && (dist[1][N - 1][M - 1] != 0)) {
+    cout << dist[1][N - 1][M - 1];
+  } else {
+    cout << (dist[0][N - 1][M - 1] < dist[1][N - 1][M - 1]
+                 ? dist[0][N - 1][M - 1]
+                 : dist[1][N - 1][M - 1]);
   }
 
   return 0;
